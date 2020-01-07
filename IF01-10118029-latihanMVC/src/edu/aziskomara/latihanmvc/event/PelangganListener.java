04d98b1f983b6768de225e294b482ed550488561
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2020-01-08 00:23:09 +0700

    Add files via upload

---
+++ IF01-10118029-latihanMVC/src/edu/aziskomara/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,20 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.aziskomara.latihanmvc.event;
+
+import edu.aziskomara.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author Azis Komara
+ * NIM   : 10118029
+ * Nama  : Azis Komara
+ * Kelas : IF-1
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+}
---
+++ IF01-10118029-latihanMVC/src/edu/aziskomara/latihanmvc/model/IF0110118029LatihanaPart1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.aziskomara.latihanmvc.model;
+
+import edu.aziskomara.latihanmvc.view.PlangganView;
+
+/**
+ *
+ * @author Azis Komara
+ * NIM   : 10118029
+ * Nama  : Azis Komara
+ * Kelas : IF-1
+ */
+public class IF0110118029LatihanaPart1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        PlangganView view = new PlangganView();
+        view.setVisible(true);
+        
+    }
+    
+}
